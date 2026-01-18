a0d3b746d4b53964390c2f67f90eaf54ec5dd976
NatsuRohit <rohitprajapati2077@gmail.com>
2026-01-18 18:40:22 +0545

    Added Database Connection

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php 
+    function dbConnect(){
+        $server = "mysql:host=localhost;dbname=online_courses_db";
+        $user = "root";
+        $password = "";
+        try{
+            $con = new PDO($server, $user, $password);
+            echo "Database Connected Successfully";
+            return $con;
+        }catch(PDOException $e){
+            die("Database Connection Failed : " .$e->getMessage());
+        }
+    }
+    dbConnect();
+ ?>
