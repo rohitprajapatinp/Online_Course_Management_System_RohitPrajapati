4b28420899365a0e17b11fa069b197d0b86f0d08
NatsuRohit <rohitprajapati2077@gmail.com>
2026-01-22 21:32:10 +0545

    Added header.php file

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Course Management System</title>
+    <link rel="stylesheet" href="../assets/css/styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="../public/index.php">Home</a></li>
+                <li><a href="../public/add.php">Add Course</a></li>
+                <li><a href="../public/edit.php">Edit Course</a></li>
+                <li><a href="../public/delete.php">Delete Course</a></li>
+                <li><a href="../public/search.php">Search Courses</a></li>
+            </ul>
+        </nav>
+    </header>
